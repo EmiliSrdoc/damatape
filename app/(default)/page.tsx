9823cbd569ddea9ae45c0044828e8c15acb06d5e
Emili Srdoc <emili@srdoc.si>
2024-11-11 15:56:23 +0100

    optimized video

--- app/(default)/page.tsx
+++ app/(default)/page.tsx
@@ -8,10 +8,12 @@ import BusinessCategories from "@/components/business-categories";
 import FeaturesPlanet from "@/components/features-planet";
 import LargeTestimonial from "@/components/large-testimonial";
 import Cta from "@/components/cta";
+import Video from "@/components/video";
 
 export default function Home() {
   return (
     <>
+      <Video />
       <Hero />
       <BusinessCategories />
       <FeaturesPlanet />
---
+++ components/video.tsx
@@ -0,0 +1,7 @@
+export default function Video() {
+  return (
+    <section>
+      <video src="video/dama-opt.m4v" autoPlay muted loop></video>
+    </section>
+  );
+}
