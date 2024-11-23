e79a66b799416a79d3f496b2c8fd7bb873696111
Emili Srdoc <emili@srdoc.si>
2024-11-23 13:30:09 +0100

    buy now top

--- components/video.tsx
+++ components/video.tsx
@@ -1,23 +1,18 @@
 export default function Video() {
   return (
     <section className="relative">
-      <a
-        className="sm:block hidden absolute top-8 right-10 z-10 cursor-pointer btn group mb-4 w-full bg-gradient-to-t from-blue-600 to-blue-500 bg-[length:100%_100%] bg-[bottom] text-white shadow hover:bg-[length:100%_150%] sm:mb-0 sm:w-auto"
-        href="https://buy.stripe.com/eVa8AB8nM4clesM144"
-        target="_blank"
-      >
-        <span className="relative inline-flex items-center">Buy Now</span>
-      </a>
       <a href="video/dama-opt.m4v">
         <video src="video/dama-opt.m4v" autoPlay muted loop></video>
       </a>
-      <div className="sm:hidden p-10">
+      <div className="p-10 flex justify-center">
         <a
-          className="cursor-pointer btn group w-full bg-gradient-to-t from-blue-600 to-blue-500 bg-[length:100%_100%] bg-[bottom] text-white shadow hover:bg-[length:100%_150%] sm:mb-0 sm:w-auto"
+          className="cursor-pointer btn group w-full bg-gradient-to-t from-blue-600 to-blue-500 bg-[length:100%_100%] bg-[bottom] text-white shadow hover:bg-[length:100%_150%] sm:mb-0 sm:w-[50%]"
           href="https://buy.stripe.com/eVa8AB8nM4clesM144"
           target="_blank"
         >
-          <span className="relative inline-flex items-center">Buy Now</span>
+          <span className="relative inline-flex items-center sm:text-xl sm:font-bold">
+            Buy Now
+          </span>
         </a>
       </div>
     </section>
